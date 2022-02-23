1b9e057d4596ea84fe70cad07d09e779c11d0793
flitiring <99330657+flitiring@users.noreply.github.com>
2022-02-23 08:39:24 -0300

    Create codigo.ino

---
+++ Aula3/codigo.ino
@@ -0,0 +1,59 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(1, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(2, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(2, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(3, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(3, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(4, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(4, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(5, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(5, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(6, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(6, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(7, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(7, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(8, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(8, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(10, HIGH);
+  delay(100); // Wait for 1 second(s)
+  digitalWrite(10, LOW);
+  delay(100); // Wait for 1 second(s)
+}
